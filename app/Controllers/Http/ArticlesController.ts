49a0afc0a2cb6efeef02636f08f7d2ccc10c1483
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-08-15 21:58:01 +0200

    feat(articles): add edit and create

--- app/Controllers/Http/ArticlesController.ts
+++ app/Controllers/Http/ArticlesController.ts
@@ -1,5 +1,6 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Article from 'App/Models/Article'
+import ArticleValidator from 'App/Validators/ArticleValidator'
 
 export default class ArticlesController {
   public async index({ request, view }: HttpContextContract) {
@@ -25,6 +26,67 @@ export default class ArticlesController {
       console.error(error)
       return view.render('errors/not-found')
     }
+
+    return view.render('article', {
+      article,
+    })
+  }
+
+  public async create({ view }: HttpContextContract) {
+    return view.render('create')
+  }
+
+  public async store({ request, view, auth }: HttpContextContract) {
+    const { user } = auth
+    const { title, content } = await request.validate(ArticleValidator)
+
+    const article = await Article.create({
+      title,
+      content,
+      ownerId: user!.id,
+    })
+
+    await article.load('owner')
+
+    return view.render('article', {
+      article,
+    })
+  }
+
+  public async edit({ view, params }: HttpContextContract) {
+    const { id } = params
+
+    let article: Article
+    try {
+      article = await Article.findOrFail(id)
+    } catch (error) {
+      console.error(error)
+      return view.render('errors/not-found')
+    }
+
+    return view.render('edit', {
+      article,
+    })
+  }
+
+  public async update({ request, view, params }: HttpContextContract) {
+    const { id } = params
+
+    let article: Article
+    try {
+      article = await Article.findOrFail(id)
+    } catch (error) {
+      console.error(error)
+      return view.render('errors/not-found')
+    }
+
+    const { title, content } = await request.validate(ArticleValidator)
+
+    article.title = title
+    article.content = content
+
+    await article.save()
+    await article.load('owner')
 
     return view.render('article', {
       article,
