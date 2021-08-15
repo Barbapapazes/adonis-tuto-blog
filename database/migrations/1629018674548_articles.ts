db5c2df6b95e7790b07f1199c79528ee512b21ee
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-08-15 11:15:28 +0200

    fix(articles): add unique to title

--- database/migrations/1629018674548_articles.ts
+++ database/migrations/1629018674548_articles.ts
@@ -6,7 +6,7 @@ export default class Articles extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('title').notNullable()
+      table.string('title').notNullable().unique()
       table.string('content').notNullable()
       table.integer('owner_id').references('id').inTable('users')
 
