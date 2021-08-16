76335476041339d49b1e26ce13981ec8e76a87db
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-08-16 18:29:32 +0200

    feat: redirect home to articles

--- start/routes.ts
+++ start/routes.ts
@@ -20,8 +20,8 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async ({ view }) => {
-  return view.render('welcome')
+Route.get('/', async ({ response }) => {
+  return response.redirect('/articles')
 })
 
 Route.post('/auth/login', 'AuthController.login')
